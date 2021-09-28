5d39d5547d4850c2dab8ded4ea99ccca2a7cea8e
unknown <DIANA_1321124132@UPTECAMAC.EDU.MX>
2021-09-28 01:39:05 -0500

    operadores

---
+++ Operadores/main1.js
@@ -0,0 +1,63 @@
+/*
+ < menor que
+> mayor que
+== igualque
+!= diferente
+<= menor o igual
+>= mayor igual
+&& Y
+// O 
+! negacion
+*/
+var x=10; // variables para comparar
+var y=20;
+var s=30;
+if(x < y){ // esta afirmacion estra como verdadera
+   console.log("x es menor a y");
+}else{
+    console.log(x +" x es ayor a "+ y)   // se le conoce como concatebar
+}
+
+var humano1= "Emmanuel";
+var humano2 ="juan";
+if(humano1==humano2){
+    console.log("son iguales");
+}else{
+    console.log("son diferentes");
+
+}
+
+var pi=3.1416;
+var radio= 10;
+if(pi !=radio){
+    console.lo+("son diferentes")
+}else{
+    console.log("son iguales");
+}
+
+var transporte=" carro";
+ 
+console.log("paso 1: salir de la entrada de mi casa");
+if( transporte == "combi"){
+    console.log("paso 2: subirme a la combi");
+}else{
+    console.log("paso 2: subirme al transporte publico ");
+}
+console.log("paso 3: bajarme en el paradero de Teecamac");
+console.log("paso 4: caminar a la entrada de la universidad");
+console.log("paso 5: caminar al aula");
+
+//EJERCICIO DE TORTILLAS
+
+var tortillas= "tortilleria";
+
+console.log("paso 1: salir de la puerta de mi casa");
+if(tortillas =="tortilleria"){
+ console.log("paso 2:ir a la tortilleria y comprar las tortillas");
+} else{
+    console.log("paso 2:ir a la tienda a comprar tortilla");
+}
+console.log("paso 3: pagar las tortillas");
+console.log("paso 4: regresar a tu casa");
+
+
---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+var mandado = "tortilla"
+console.log("Paso 1: que te pidan ir por las tortillas");
+if(mandado =="tortilla"){
+    console.log("paso 2: pedir el dinero");
+    console.log("paso 3: salir de la puerta de tu casa");
+    console.log("paso 4: caminar hacia la tortilleria");
+    console.log("paso 5: comprar las tortillas");
+    console.log("paso 6: regresar a tu casa");
+}else{
+    console.log(" paso 2:ir a la tienda");
+    console.log("paso 3: comprar tortillas");
+}
